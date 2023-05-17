b722b324ae989f69369b66439519d3f2e9cdef7d
Ángel Nieto Burgos <anb061@inlumine.ual.es>
2023-05-17 21:15:21 +0200

    Modificacion test

--- test/org/hmis/JsonReaderTest.java
+++ test/org/hmis/JsonReaderTest.java
@@ -1,27 +1,43 @@
 package org.hmis;
+import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.stream.Stream;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
 class JsonReaderTest {
-
-	@Test
-	void testLeerCochesJSON() {
-		String ruta = "data/coches.json";
+	static Stream<Arguments> Coches(){
+		String Marca="Toyota";
+		String Modelo="Corolla";
+		int año=2022;
+		int precio=22000;
+		return Stream.of(
+				
+				Arguments.of(Marca, Modelo,año,precio));
+		}
+	@ParameterizedTest
+	@CsvSource("data/coches.json")
+	void testLeerCochesJSON(String ruta) {
 		Coche [] coches = JsonReader.leerCochesJSON(ruta);
 		assertEquals (4, coches.length);
 	}
 
-	@Test
-	void testLeerCochesJSONprimero() {
+	@ParameterizedTest
+	@MethodSource("Coches")
+	void testLeerCochesJSONprimero(String Marca,String Modelo,int año,int precio) {
 		String ruta = "data/coches.json";
-		Coche primero = new Coche ("Toyota", "Corolla", 2022, 22000);
+		;
 		Coche [] coches = JsonReader.leerCochesJSON(ruta);
-		assertEquals(primero, coches[0]);
-		assertTrue (primero.equals(coches[0]));
-		assertTrue (coches[0].equals(primero));
+		assertEquals( new Coche (Marca,Modelo ,año ,precio ), coches[0]);
+		assertTrue ( new Coche (Marca,Modelo ,año ,precio ).equals(coches[0]));
+		assertTrue (coches[0].equals( new Coche (Marca,Modelo ,año ,precio )));
 	}
 
 
